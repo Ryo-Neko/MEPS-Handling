84e282e0491f41dfaa7ce983d0500ba68d761202
Ryo Kaneko <deepkneko@gmail.com>
2024-10-16 16:30:47 +0900

    fix Docker env

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -it --mount type=bind,source=.,target=/home/worker/calc -p 8888:8888 python:3.10-slim /bin/bash
+docker run -it -u worker --mount type=bind,source=.,target=/home/worker/calc -p 8888:8888 python:3.10-slim /bin/bash
 
